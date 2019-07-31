afd043a4c4bbee5b1bfd3da9df1a0fe3fc163afa
d-mv <d.dash.mv@gmail.com>
2019-08-01 00:00:04 +0300

    done

--- src/destructure.ts
+++ src/destructure.ts
@@ -1,8 +1,38 @@
-import express from 'express';
+import axios from 'axios';
+import dotenv from 'dotenv';
+import { RequestQuery } from './reporter';
 
-const destructureRequest = (request: express.Request) => {
-  console.log(request);
-  return '';
+const dotEnv = dotenv.config();
+
+const token: string = process.env.IPINFO;
+
+interface IpInfoData {
+  data: {
+    ip: string;
+    city: string;
+    region: string;
+    country: string;
+    loc: string;
+    hostname: string;
+    org: string;
+  };
+}
+
+export interface Reply {
+  location: string;
+  data: string;
+  time: string;
+}
+
+const destructureRequest = async (request: RequestQuery) => {
+  const ipInfo: IpInfoData = await axios(
+    `https://ipinfo.io/${request.ip}/json?token=${token}`
+  );
+  return {
+    location: `${ipInfo.data.city}, ${ipInfo.data.country}`,
+    data: ipInfo.data,
+    time: request.time
+  };
 };
 
 export default destructureRequest;
--- src/reporter.ts
+++ src/reporter.ts
@@ -1,40 +1,59 @@
-import express from 'express';
+import dotenv from 'dotenv';
 import nm from 'nodemailer';
-import destructureRequest from './destructure';
+import destructureRequest, { Reply } from './destructure';
 
-const login:string = process.env.LOGIN;
-const pass:string = process.env.PASS;
-const user:string = process.env.USER;
+const dotEnv = dotenv.config();
 
-const report = (request: express.Request) => {
-  // let transporter = nm.createTransport({
-  //   host: 'smtpout.secureserver.net',
-  //   port: 465,
-  //   secure: true,
-  //   auth: {
-  //     user: login,
-  //     pass: pass
-  //   }
-  // });
+const login: string = process.env.LOGIN;
+const pass: string = process.env.PASS;
+const recipient: string = process.env.RECIPIENT;
 
-  const text = destructureRequest(request);
+const formText = (reply: Reply): string =>
+  `There is a new request.
+From:
+${JSON.stringify(reply.data)}
+On: ${reply.time}`;
+
+export interface RequestQuery {
+  ip: string;
+  time: string;
+}
+
+const report = async (request: RequestQuery) => {
+  let transporter = nm.createTransport({
+    host: 'smtp.yandex.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: login,
+      pass: pass
+    }
+  });
+
+  const reply: any = await destructureRequest(request);
+  const text = formText(reply);
 
   const mailOptions = {
     from: {
       name: 'Report',
       address: login
     },
-    to: user,
-    subject: 'New Request //',
-    text,
+    to: recipient,
+    subject: `New Request from ${reply.location}`,
+    text
     // html
   };
-  // transporter.sendMail(mailOptions, (error: any, info: any) => {
-  //   if (error) {
-  //     console.log(error);
-  //   }
-  //   console.log('Message %s sent: %s', info.messageId, info.response);
-  // });
+  let result = '';
+  transporter.sendMail(mailOptions, (error: any, info: any) => {
+    if (error) {
+      result = error;
+      console.log(error);
+    } else {
+      result = info;
+      console.log('Message %s sent: %s', info.messageId, info.response);
+    }
+  });
+  return result;
 };
 
-export default report
+export default report;
